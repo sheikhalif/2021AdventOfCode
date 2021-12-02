19d0119d4e71d96f9675f054e0a0e3b95406d90d
Sheikh Alif <salif30@stuy.edu>
2021-12-02 11:43:36 -0500

    finished day 1

--- Day1.java
+++ Day1.java
@@ -1,3 +1,26 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class Day1{
-  
+  public static void main(String[] args) {
+    String fileName = args[0];
+    int total = 0;
+    try {
+      File file = new File(fileName);
+      Scanner input = new Scanner(file);
+      int initial = 1000000;
+      while (input.hasNextLine()){
+        int num = Integer.parseInt(input.nextLine());
+        if (num > initial){
+          total++;
+        }
+        initial = num;
+      }
+    }
+    catch (FileNotFoundException ex) {
+      System.out.println("Not a valid file name");
+    }
+    System.out.println(total);
+  }
 }
